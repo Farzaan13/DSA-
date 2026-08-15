7a979920fe74b138d0fd135fcf6b96779e5bb7f6
Farzaan_13 <mohdfarzaan1307@gmail.com>
2026-08-15 19:55:06 +0530

    Time: 0 ms (100%), Space: 45.3 MB (69.93%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+        
+        ListNode i = head;
+        ListNode j = head;
+
+        while(j != null){
+            if(i.val==j.val){
+                j = j.next;
+            }
+            else{
+                i.next = j;
+                i = j;
+            }
+        }
+        i.next = j;
+
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
