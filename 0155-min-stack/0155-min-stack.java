ff2e91b3cd339b0a84c8ce339ccb5a095bfeb081
Farzaan_13 <mohdfarzaan1307@gmail.com>
2026-08-31 14:01:26 +0530

    Time: 31 ms (84.07%), Space: 102.3 MB (26.33%) - LeetHub - Time: O(1), Space: O(n)

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,42 +1,79 @@
-class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minst;
+// USING 1 STACK AND A VARIABLE MIN FOR GETMIN FUNCTION
 
+class MinStack {
+    Stack<Long> st;
+    long min;
     public MinStack() {
         st = new Stack<>();
-        minst = new Stack<>();
-        
+        min = Long.MAX_VALUE;
     }
     
-    public void push(int value) {
-        st.push(value);
-        if(minst.size()==0 || st.peek() < minst.peek()) minst.push(st.peek());
-        else minst.push(minst.peek());
-        
+    public void push(int val) {
+        long value = (long)val;
+        if(st.size()==0) min = value;
+
+        if(value >= min) st.push(value);
+        else{ 
+            st.push(value + (value-min));
+            min = value;
+        }
     }
     
     public void pop() {
+        if(st.peek() < min){ 
+            min = min + (min - st.peek());
+        }
         st.pop();
-        minst.pop();
     }
     
-    public int top() {
-        return st.peek();
+    public int top() { 
+        long peek = st.peek();
+        if(peek < min){ 
+            return (int)min;
+        }
+        else return (int)peek;
     }
     
     public int getMin() {
-        return minst.peek();
+        return (int)min;
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(value);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+
+//  USING TWO STACKS
+
+
+// class MinStack {
+//     Stack<Integer> st;
+//     Stack<Integer> minst;
+
+//     public MinStack() {
+//         st = new Stack<>();
+//         minst = new Stack<>();
+        
+//     }
+    
+//     public void push(int value) {
+//         st.push(value);
+//         if(minst.size()==0 || st.peek() < minst.peek()) minst.push(st.peek());
+//         else minst.push(minst.peek());
+        
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//         minst.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek();
+//     }
+    
+//     public int getMin() {
+//         return minst.peek();
+//     }
+// }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
