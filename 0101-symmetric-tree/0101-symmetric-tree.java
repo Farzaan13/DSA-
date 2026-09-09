fdbe5b4510796e400c87e96e5bbaf9df3d5db614
Farzaan_13 <mohdfarzaan1307@gmail.com>
2026-09-10 00:16:34 +0530

    Time: 0 ms (100%), Space: 43.7 MB (33.42%) - LeetHub - Time: O(N), Space: O(H)

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public void mirror(TreeNode root){
+        if(root == null) return;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        mirror(root.left);
+        mirror(root.right);
+    }
+    public boolean isSameTree(TreeNode p,TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        mirror(root.left);
+        return isSameTree(root.left,root.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
