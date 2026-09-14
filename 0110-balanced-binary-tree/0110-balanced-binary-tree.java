3c3da5fd86eecee50065afe55779e320f9fa5d68
Farzaan_13 <mohdfarzaan1307@gmail.com>
2026-09-14 16:44:05 +0530

    Time: 0 ms (100%), Space: 45.8 MB (10.71%) - LeetHub - Time: O(n), Space: O(h)

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,18 +1,53 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    // METHOD 1
+    static boolean flag;
     public boolean isBalanced(TreeNode root) {
         if(root==null) return true;
+        flag = true;
+        levels(root);
+        return flag;
+    }
+    private int levels(TreeNode root){
+        if(root == null) return 1;
         int left = levels(root.left);
         int right = levels(root.right);
+        if(Math.abs(left-right)>1){
+            flag = false;
+        }
+        return 1+ Math.max(left,right);
 
-        if(Math.abs(left-right)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    private int levels(TreeNode root){
-        if(root== null) return 1;
-        return 1+ Math.max(levels(root.left),levels(root.right));
-    }
+//  METHOD 2
+
+    // public boolean isBalanced(TreeNode root) {
+    //     if(root==null) return true;
+    //     int left = levels(root.left);
+    //     int right = levels(root.right);
+
+    //     if(Math.abs(left-right)>1) return false;
+    //     return isBalanced(root.left) && isBalanced(root.right);
+    // }
+
+    // private int levels(TreeNode root){
+    //     if(root== null) return 1;
+    //     return 1+ Math.max(levels(root.left),levels(root.right));
+    
 }
 
 // Synced seamlessly with LeetHub Pro
